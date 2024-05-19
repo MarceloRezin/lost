eebc9d452bdc009e51dbecf358c546644b5a6ab6
Marcelo Rezin <marcelo.rezin@gmail.com>
2024-05-19 16:55:37 -0300

    Imprime um string utilizando a função do BIOS

--- boot.asm
+++ boot.asm
@@ -1,21 +1,39 @@
-org 0x7c00
+%define ORIGEM 0x7c00
+
+org ORIGEM
 bits 16
 
 main:
-	mov al, 'O'
-	mov ah, 0x0e
-	mov bh, 0
+	xor ax, ax
+	mov ds, ax
+	mov es, ax
+
+	;pilha
+	mov ss, ax
+	mov sp, [ORIGEM]
+
+clear_screen:
+	mov al, 0x03
 	int 0x10
 
-	mov al, 'I'
-        mov ah, 0x0e
-        mov bh, 0
+print_str:
+        mov ax, 0x1301
+	mov bx, 0x02
+	mov cx, pad-msg
+	xor dx, dx
+	mov bp, msg
 	int 0x10
 
 halt:
 	;parada da CPU
+	cli
 	hlt
-	jmp halt
 
-times 510-($-$$) db 0
-dw 0xaa55
+msg:
+	db 'Carregando SO ...'
+
+pad:
+	times 510-($-$$) db 0
+
+bios_sign:
+	dw 0xaa55
